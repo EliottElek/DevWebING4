027f761feddf73d3b20ef3c632550f64efbfeaf6
EliottElek <64375473+EliottElek@users.noreply.github.com>
2021-09-20 17:51:28 +0200

    feat: new module format

--- lab2/metrics.js
+++ lab2/metrics.js
@@ -1,8 +1,16 @@
-module.exports = {
+// module.exports = {
+//     get: (callback) => {
+//       callback(null, [
+//         { timestamp: new Date('2013-11-04 14:00 UTC').getTime(), value:12}
+//       , { timestamp: new Date('2013-11-04 14:30 UTC').getTime(), value:15}
+//       ])
+//     }
+//   }
+  module.exports = {
     get: (callback) => {
       callback(null, [
-        { timestamp: new Date('2013-11-04 14:00 UTC').getTime(), value:12}
-      , { timestamp: new Date('2013-11-04 14:30 UTC').getTime(), value:15}
+        { name: "Anthonin", lastname:"Dufourneur"}
+      , { name: "Ulysse", lastname:"DeLaChance"}
       ])
     }
   }
